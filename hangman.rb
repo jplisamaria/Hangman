cc6c1fb0b6b01f423f5423a594f602c4a8b11840
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-03-01 22:01:44 -0500

    Done...I think

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,16 @@
 #### DECLARATIONS ####
-word_string = "test"
+words = []
+words = IO.readlines("words.txt")
+word_string = words.sample.strip
 word = word_string.split(//)
-letters_guessed = []
+guesses = []
 ALLOWED_GUESSES = 6
 remaining_guesses = ALLOWED_GUESSES
 
 #### METHODS ####
-def print_word_in_progress(word, letters_guessed)
+def print_word_in_progress(word, guesses)
   word.each do |letter|
-    if letters_guessed.include? letter
+    if guesses.include? letter
       print "#{letter} "
     else print "_ "
     end
@@ -16,38 +18,34 @@ def print_word_in_progress(word, letters_guessed)
 end
 
 #### HEADER ####
-system "clear"
-puts
 intro = "This is LAME HANGMAN...the awesomest hangman you've every played."
-puts "*" * intro.length
+long_bar = "*" * intro.length 
+short_bar = "*" * (0.66 * intro.length)
+puts long_bar
 puts intro
 puts "Enter your guess one letter a time."
-puts "*" * intro.length
+puts long_bar
 3.times {puts}
 
 #### MAIN PROGRAM ####
 while remaining_guesses > 0
-  puts "*" * (0.66 * intro.length)
+  puts short_bar
   puts "#{remaining_guesses} guesses left."
   print "You've guessed the following letters: "
-  letters_guessed.uniq.sort.each do |letter|
-    print "#{letter}  "
-  end 
+  guesses.uniq.sort.each { |letter| print "#{letter}  "} 
   3.times {puts}
-  print_word_in_progress(word, letters_guessed)
+  print_word_in_progress(word, guesses)
   2.times {puts}
   
   print "What's your guess?\n> "
   guess = gets.strip.downcase
   3.times {puts}
-#  system "clear"
 
   if word.include? guess
-    letters_guessed << guess
-#    puts
+    guesses << guess
     puts "Good guess."
 
-    if word.uniq - letters_guessed == []
+    if word.uniq - guesses == []
       2.times {puts}
       puts "You win!  The word was '#{word_string}'."
       puts "Contratulations.   WOO HOO!!!!"
@@ -56,10 +54,9 @@ while remaining_guesses > 0
     end
 
   else
- #   puts
     puts "Wrong."
-    unless letters_guessed.include? guess
-      letters_guessed << guess
+    unless guesses.include? guess
+      guesses << guess
       remaining_guesses = remaining_guesses - 1
       if remaining_guesses == 0 
         puts "Sorry, the word was '#{word_string}'."
